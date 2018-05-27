26dc6c026c174f68eaa9b758f4408c61e2398a0a
Noel Díaz Rodríguez <noeldiazro@gmail.com>
2018-05-27 15:42:31 +0200

    Base Inbox fragment added

---
+++ app/src/main/java/es/montanus/catchat/InboxFragment.java
@@ -0,0 +1,18 @@
+package es.montanus.catchat;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class InboxFragment extends Fragment {
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_inbox, container, false);
+    }
+
+}
